ba7643f84b4f411ee1164752e5e2f569e1a5a041
Alexandr Morev <frontend/>
2022-11-20 16:15:24 +0000

    Fix frontend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -61,7 +61,7 @@ function App() {
         .checkToken({ token })
         .then((res) => {
           if (res) {
-            setEmail(res.data.email);
+            setEmail(res.email);
             setLoggedIn(true);
           }
         })
@@ -70,7 +70,7 @@ function App() {
   }
 
   function handleCardLike(card) {
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((userId) => userId === currentUser._id);
 
     api
       .changeLikeCardStatus(card._id, !isLiked)
